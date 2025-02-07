eea23fff64874bc1eb7dcfe04ec21bd38a1fa79c
JasonTMarino <jasontmarino@gmail.com>
2025-02-07 11:41:21 -0500

    first draft of Pybank uploaded to main.py

--- PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -2,17 +2,15 @@
 import csv
 import os
 
-
 # Files to load and output (updated with correct file paths)
-file_to_load = "Resources/budget_data.csv"  # Input file path
-file_to_output = "analysis/budget_analysis.txt"  # Output file path
+file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
 
 # Variables to track the financial data
 total_months = 0
 total_net = 0
 
 # Opens and reads the csv, skips the header row
-with open(file_to_load,"r") as financial_data:
+with open(file_to_load) as financial_data:
     reader = csv.reader(financial_data)
     header = next(reader)
-print(header)
